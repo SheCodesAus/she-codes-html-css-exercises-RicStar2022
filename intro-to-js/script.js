a41312dd78a968ff2fdbd510f149ece4d11b7f95
RicStar2022 <ricci.lee@bigpond.com>
2022-04-26 19:32:27 +1000

    Introduction to JS
    JS class.

--- intro-to-js/script.js
+++ intro-to-js/script.js
@@ -1,5 +1,18 @@
 window.onload = function() {
     // start
+let starterDiv = document.getElementById('starter-div')
+let firstHeading = document.createElement('h2')
+firstHeading.innerText = 'This is an H2 Created with Javascript!'
+firstHeading.classList.add('js-header')
+
+starterDiv.appendChild(firstHeading)
+
+let secondHeading = document.createElement('h2');
+secondHeading.innerText = 'This is another H2 created with JS';
+secondHeading.classList.add('js-header');
+secondHeading.id = 'second-h2';
+
+starterDiv.appendChild(secondHeading)
 
     // end
 }
@@ -8,13 +21,14 @@ window.onload = function() {
 function showName(){
     console.log('showName');
     // start
-
+let name = document.getElementById('name').value; 
+document.getElementById('greeting-text').innerText = 'Hi ' + name;
     // end
 }
 
-function changeColour(colour) {
+function changeColour(parameter) {
     // start
-
+document.body.style.backgroundColor = parameter; 
     // end
 }
 
@@ -24,6 +38,7 @@ function resetColour() {
 
 function toggleLightbulb() {
     // start
-
+let lightbulb = document.getElementById('light');
+lightbulb.classList.toggle("on");
     // end
 }
--- keyboard/script.js
+++ keyboard/script.js
@@ -1,23 +1,42 @@
 window.onload = function() {
     let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
     // start
+let keyboard = document.getElementById('keyboard');
 
     // end
 
     alphabet.forEach(letter => {
         // start
-
+let keyButton = document.createElement('input')
+keyButton.type = 'button';
+keyButton.value = letter;
+keyButton.classList.add('key');
+keyButton.id = letter;
+keyboard.appendChild(keyButton);
+keyButton.addEventListener('click', event => {
+    highlightLetter(event.target.id)
+})
         // end
     });
 }
 
 // start
-
+document.addEventListener('keydown', event => {
+    highlightLetter(event.key)
+})
 // end
 
 
 function highlightLetter(letter){
     // start
-
+console.log(letter)
+let output = document.getElementById('output-text');
+    output.innerText = output.innerText + letter;
+    let key = document.getElementById(letter); 
+    key.classList.toggle('selected');
+    setTimeout(function() {
+        key.classList.toggle('selected');
+    }, 150)
+    
     // end
 }
